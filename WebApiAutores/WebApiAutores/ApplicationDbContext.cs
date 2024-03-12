0ab77f33c28d8b0a3bee6633e9c4954b6c6cf66f
Giovanni Tapia <ryukaj1_@hotmail.com>
2024-03-12 09:00:02 -0600

    Commit inicial

---
+++ WebApiAutores/WebApiAutores/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Nombre de la tabla a crear
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>(){ 
+             new Autor() { Id = 1, Nombre = "Gio" },
+             new Autor() { Id = 2, Nombre = "Monty" },
+            };
+        }
+    }
+}
