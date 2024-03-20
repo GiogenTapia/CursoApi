c8205fcf2ad226904708b192fea7b7f421aaf393
Giovanni Tapia <ryukaj1_@hotmail.com>
2024-03-20 09:55:35 -0600

    Agregando tipos de validaciones

--- WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -5,7 +5,7 @@ using WebApiAutores.Entidades;
 namespace WebApiAutores.Controllers
 {
     [ApiController]
-    [Route("api/autores")]
+    [Route("api/autores")] // api/autores
     public class AutoresController : ControllerBase
     {
         private readonly ApplicationDbContext context;
@@ -15,19 +15,65 @@ namespace WebApiAutores.Controllers
             this.context = context;
         }
 
-        [HttpGet]
+        [HttpGet] // api/autores
+        [HttpGet("listado")] //  api/autores/listado
+        [HttpGet("/listado")]// /listado
         public async Task<ActionResult<List<Autor>>> Get()
         {
-           return await context.Autores.Include(x=> x.Libros).ToListAsync();
+            return await context.Autores.Include(x => x.Libros).ToListAsync();
+        }
+
+        // Para mandar datos en nuestos endpoints ponemos entre {}
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return autor;
+        }
+
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get([FromRoute] string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(autor);
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post(Autor autor)
+        public async Task<ActionResult> Post([FromBody] Autor autor)
         {
+            var existeAutorNombre = await context.Autores.AnyAsync(x => x.Nombre == autor.Nombre);
+
+            if (existeAutorNombre)
+            {
+                return BadRequest($"Ya existe un autor con el nombre {autor.Nombre}");
+            }
+
             context.Add(autor);
             await context.SaveChangesAsync();
             return Ok();
         }
+
+
+        [HttpGet("primero")] // api/autores/primero
+        public async Task<ActionResult<Autor>> PrimerAutor([FromHeader] int miValor)
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+
 
         [HttpPut("{id:int}")] // api/autores/1
         public async Task<ActionResult> Put(Autor autor, int id)
--- WebApiAutores/WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/WebApiAutores/Entidades/Autor.cs
@@ -1,9 +1,42 @@
-﻿namespace WebApiAutores.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApiAutores.Validaciones;
+
+namespace WebApiAutores.Entidades
 {
-    public class Autor
+    public class Autor: IValidatableObject
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength:120, ErrorMessage ="El campo {0} no debe de tener mas de {1} caracteres")]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public List<Libro> Libros { get; set; }
+        [NotMapped]
+        public int Menor { get; set; }
+        [NotMapped]
+        public int Mayor { get; set; }
+
+
+        // Para que se ejecute esto, debe de validarse primero los de atributos de nuesta clase
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula",
+                        new string[] { nameof(Nombre) });
+                }
+            }
+
+            if (Menor > Mayor)
+            {
+                yield return new ValidationResult("Este valor no puede ser mas grande que el campo Mayor",
+                    new string[] {nameof(Menor) } );
+            }
+        }
     }
 }
