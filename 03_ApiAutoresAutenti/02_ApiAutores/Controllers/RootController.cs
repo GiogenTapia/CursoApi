046cf7cbd9929b7ec4775ff056475364fe47f232
GiogenTapia <tapiamgio@gmail.com>
2024-04-12 08:47:23 -0600

    Implementando HATEOOS

---
+++ 03_ApiAutoresAutenti/02_ApiAutores/Controllers/RootController.cs
@@ -0,0 +1,23 @@
+﻿using _02_ApiAutores.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_ApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class RootController:ControllerBase
+    {
+        //Prueba de hateos
+        [HttpGet(Name = "ObtenerRoot")]
+        public ActionResult<IEnumerable<DatoHATEOAS>> Get()
+        {
+            var datosHateos = new List<DatoHATEOAS>();
+            //Construcción de nuestro objeto DTO
+            //Estos atributos se realizan para hacer la referencia a la misma URL
+            datosHateos.Add(new DatoHATEOAS(enlace: Url.Link("ObtenerRoot", new {}) 
+                ,descripcion: "self", "GET"));  
+
+            return datosHateos;
+        }
+    }
+}
---
+++ 03_ApiAutoresAutenti/02_ApiAutores/DTOs/Recurso.cs
@@ -0,0 +1,9 @@
+﻿namespace _02_ApiAutores.DTOs
+{
+    public class Recurso
+    {
+
+        //Aqui siempre sera un listado vacio
+        public List<DatoHATEOAS> Enlaces { get; set; } = new List<DatoHATEOAS>();
+    }
+}
