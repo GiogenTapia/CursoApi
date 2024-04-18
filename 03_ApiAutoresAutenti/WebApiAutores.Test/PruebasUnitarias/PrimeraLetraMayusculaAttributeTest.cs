381b38673916b2626b7304d0c18cf08c17bc62a7
GiogenTapia <tapiamgio@gmail.com>
2024-04-18 09:10:14 -0600

    Agregando mas pruebas

--- 03_ApiAutoresAutenti/WebApiAutores.Test/PruebasUnitarias/PrimeraLetraMayusculaAttributeTest.cs
+++ 03_ApiAutoresAutenti/WebApiAutores.Test/PruebasUnitarias/PrimeraLetraMayusculaAttributeTest.cs
@@ -20,5 +20,38 @@ namespace WebApiAutores.Test.PruebasUnitarias
             //Verificación
             Assert.AreEqual("La primera letra debe ser mayuscula", resultado.ErrorMessage);
         }
+
+
+        [TestMethod]
+        public void ValorNulo_NoDevuelveError()
+        {
+            //Preparación
+            var primeraLetraMayuscula = new PrimeraLetraMayusculaAttribute();
+            string valor = null;
+            var valContext = new ValidationContext(new { Nombre = valor });
+
+            //Ejecución
+            var resultado = primeraLetraMayuscula.GetValidationResult(valor, valContext);
+
+            //Verificación
+            Assert.IsNull(resultado);
+        }
+
+
+        [TestMethod]
+        public void ValorConPrimeraLetraMayuscula_NoDevuelveError()
+        {
+            //Preparación
+            var primeraLetraMayuscula = new PrimeraLetraMayusculaAttribute();
+            string valor = "Giovanni";
+            var valContext = new ValidationContext(new { Nombre = valor });
+
+            //Ejecución
+            var resultado = primeraLetraMayuscula.GetValidationResult(valor, valContext);
+
+            //Verificación
+            Assert.IsNull(resultado);
+        }
     }
+
 }
